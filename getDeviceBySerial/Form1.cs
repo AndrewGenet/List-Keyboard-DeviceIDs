9e931c7b5d12095ab8df5fadf9caa258d5baf38d
Andrew Genet <awg6@zips.uakron.edu>
2017-11-15 12:06:46 -0500

    Add project files.

---
+++ getDeviceBySerial/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.IO;
+using System.Windows.Input;
+using System.Runtime.InteropServices;
+
+namespace getDeviceBySerial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            obSearch();
+        }
+
+        private void obSearch()
+        {
+            ManagementObjectCollection mbsList = null;
+            ManagementObjectSearcher mbs = new ManagementObjectSearcher("Select * From Win32_Keyboard");
+            mbsList = mbs.Get();
+            
+            foreach (ManagementObject mo in mbsList)
+            {
+                    listBox1.Items.Add(mo["DeviceID"].ToString());
+            }
+            
+        }
+
+    }
+}
